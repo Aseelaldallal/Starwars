feac1a4119d6b5d8d64fced5bb76fbf907570482
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-05-01 22:07:07 -0400

    enable tab navigation

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  characters = [
+    {name: 'Luke Skywalker', side: 'Light'},
+    {name: 'Darth Vader', side: 'Dark'}
+  ];
+
+  chosenSide = 'all';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  chooseSide(side) {
+    this.chosenSide = side;
+  }
+
+  getCharacters(charInfo) {
+    if (this.chosenSide === 'all') {
+      return this.characters;
+    }
+    return this.characters.filter( (char) => {
+      return char.side === this.chosenSide;
+    });
   }
 
 }
