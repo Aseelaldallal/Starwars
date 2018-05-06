fc23d0d66f041960840fbfeacd62bd48110f06f1
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-05-06 17:17:06 -0400

    remove logic - using routing now

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,30 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { StarWarsService } from '../services/Starwars.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css']
 })
-
 export class TabsComponent implements OnInit {
+  constructor() {}
 
-  chosenSide = 'all';
-  swService: StarWarsService;
-
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
-  }
-
-  ngOnInit() {
-  }
-
-  getCharacters() {
-    return this.swService.getCharacters(this.chosenSide);
-  }
-
-  chooseSide(side) {
-    this.chosenSide = side;
-  }
-
+  ngOnInit() {}
 }
