df70af6e00231425034236d46b5e069cf98fa4ae
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-05-22 20:55:35 -0400

    seperate char module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ListComponent } from './list/list.component';
+
 import { TabsComponent } from './tabs/tabs.component';
-import { CreateCharacterComponent } from './create-character/create-character.component';
+import { ListComponent } from './list/list.component';
 
 const routes = [
   { path: 'characters', component: TabsComponent, children: [
-    { path: '', redirectTo: 'all', pathMatch: 'full'},
-    { path: ':side', component: ListComponent},
-  ]},
-  { path: 'new', component: CreateCharacterComponent},
-  { path: '**', redirectTo: '/characters/all' }
+    { path: '', redirectTo: 'all', pathMatch: 'full' },
+    { path: ':side', component: ListComponent }
+  ] },
+  { path: 'new', loadChildren: './create-character/create-character.module#CreateCharacterModule' },
+  { path: '**', redirectTo: '/characters' }
 ];
 
 @NgModule({
@@ -21,5 +21,4 @@ const routes = [
     RouterModule
   ]
 })
-
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { ListComponent } from './list/list.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 import { StarWarsService } from './services/Starwars.service';
 import { LogService } from './services/log.service';
-import { CreateCharacterComponent } from './create-character/create-character.component';
 import { HeaderComponent } from './header/header.component';
-import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent,
-    ListComponent,
     TabsComponent,
-    CreateCharacterComponent,
+    ListComponent,
+    ItemComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     AppRoutingModule
   ],
